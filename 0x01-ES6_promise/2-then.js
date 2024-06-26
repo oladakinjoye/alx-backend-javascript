2422234948a0d3899da9c6f1c9a010e764b72544
oladakinjoye <oladakinjoye@gmail.com>
2024-06-26 11:55:47 -0700

    update 1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then((response) => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
+      return {status: 200,body: 'success',};
     })
     .catch((error) => {
       console.log('Got a response from the API');
