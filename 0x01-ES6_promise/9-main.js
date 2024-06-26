e22fd527fb8e0456aab8687e41ad9f7819ff900e
oladakinjoye <oladakinjoye@gmail.com>
2024-06-26 16:08:29 -0700

    Throw an error

---
+++ 0x01-ES6_promise/9-main.js
@@ -0,0 +1,6 @@
+bob@dylan:~$ cat 9-main.js
+import guardrail from './9-try';
+import divideFunction from './8-try';
+
+console.log(guardrail(() => { return divideFunction(10, 2)}));
+console.log(guardrail(() => { return divideFunction(10, 0)}));
