431b77cd85d94eec45914dde11cd9ec41e3871c6
oladakinjoye <oladakinjoye@gmail.com>
2024-07-02 09:58:30 -0700

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Create a list of students.
+ * @author Babatunde Oladunjoye <https://github.com/oladakinjoye>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
